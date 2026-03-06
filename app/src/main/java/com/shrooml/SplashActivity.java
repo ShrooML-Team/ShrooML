07571824b193f5d92e7ea15a364f479f4aa0d119
Maelig <maelig.pesco@gmail.com>
2026-03-06 10:32:27 +0100

    feat: splashScreen (WIP - cleanining files TODO)

---
+++ app/src/main/java/com/shrooml/SplashActivity.java
@@ -0,0 +1,49 @@
+package com.shrooml;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SplashActivity extends Activity {
+
+    private static final int SPLASH_DURATION = 10000; // 2 secondes
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setBackgroundDrawable(null); // ← clé magique
+
+        setContentView(R.layout.activity_splash);
+
+        View logo = findViewById(R.id.logo);
+        View title = findViewById(R.id.title);
+        View loading = findViewById(R.id.loadingText);
+
+        // Fade logo
+        logo.animate()
+                .alpha(1f)
+                .setDuration(2200)
+                .start();
+
+        // Fade titre légèrement après
+        title.animate()
+                .alpha(1f)
+                .setStartDelay(400)
+                .setDuration(2200)
+                .start();
+
+        // Fade loading encore après
+        loading.animate()
+                .alpha(1f)
+                .setStartDelay(800)
+                .setDuration(2200)
+                .start();
+
+        new android.os.Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, QuizActivity.class));
+            finish();
+        }, 6000);
+    }
+}
