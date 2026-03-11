579f58860b5b27423a24dac618d27a28f922e8b3
meo <meo.prn@outlook.fr>
2026-03-11 10:58:35 +0100

    fix: layout + crash

--- app/src/main/java/com/shrooml/services/api/OAuthRetrofitClient.java
+++ app/src/main/java/com/shrooml/services/api/OAuthRetrofitClient.java
@@ -4,8 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class OAuthRetrofitClient {
-    // Pour émulateur Android : 10.0.2.2 correspond à localhost de la machine hôte
-    private static final String BASE_URL = "http://10.0.2.2:8000/";
+    private static final String BASE_URL = "https://shroomleur.shrooml.duckdns.org/";
     private static Retrofit retrofit;
 
     public static OAuthApi getApi() {
