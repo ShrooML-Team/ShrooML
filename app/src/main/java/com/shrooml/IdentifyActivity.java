f1a116d84543645f33b6b69e721913583f5fa6ae
Beeztoz <bilal.mezrhab887@outlook.fr>
2026-03-28 00:21:33 +0100

    HotFix IHM IdentifyActivity

--- app/src/main/java/com/shrooml/IdentifyActivity.java
+++ app/src/main/java/com/shrooml/IdentifyActivity.java
@@ -1,58 +1,25 @@
 package com.shrooml;
 
 
-import static android.widget.Toast.LENGTH_LONG;
-import static android.widget.Toast.LENGTH_SHORT;
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.Toast;
-import androidx.core.content.ContextCompat;
-
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.shrooml.R;
 import com.shrooml.fragments.CameraFragment;
 import com.shrooml.fragments.FormFragment;
 import com.shrooml.fragments.ResultFragment;
-import com.shrooml.models.IdentificationEntity;
-import com.shrooml.services.KindwiseService;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 public class IdentifyActivity extends AppCompatActivity {
-    private ActivityResultLauncher<Uri> takePhotoLauncher;
-    private File photoFile;
 
     private ImageButton btnToggleMode;
-    private ImageButton btnTakePhoto;
-    private String mushroomIdentify;
-    private ImageView mushroomImage;
-
-    private KindwiseService identify_API;
-
-    private Double accuracyIdentify;
 
     private boolean isCameraMode = true;
-    private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -132,124 +99,7 @@ public class IdentifyActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission accordée → lancer la caméra
-                launchCamera();
-            } else {
-                // Permission refusée
-                Toast.makeText(this, "Permission caméra requise pour prendre une photo", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    private File uriToFile(Uri uri) throws IOException {
-        InputStream inputStream = getContentResolver().openInputStream(uri);
-        File tempFile = File.createTempFile("mushroom_", ".jpg", getCacheDir());
-        OutputStream outputStream = new FileOutputStream(tempFile);
-
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = inputStream.read(buffer)) > 0) {
-            outputStream.write(buffer, 0, length);
-        }
-
-        outputStream.close();
-        inputStream.close();
-        return tempFile;
-    }
-
-    // Initialiser le launcher dans onCreate ou dans une fonction setup
-    private void setupCameraLauncher() {
-        takePhotoLauncher = registerForActivityResult(
-                new ActivityResultContracts.TakePicture(),
-                result -> {
-                    if (result) {
-                        Glide.with(IdentifyActivity.this)
-                                .load(photoFile)
-                                .centerCrop()
-                                .into(mushroomImage);
-
-                        // Envoyer à ton API d'identification
-                        callApi(photoFile);
-                    }
-                }
-        );
-    }
-
-    //Créer un fichier temporaire pour la caméra
-    private File createImageFile() throws IOException {
-        return File.createTempFile("mushroom_photo_", ".jpg", getCacheDir());
-    }
-
-    //Bouton caméra
-    private void setupCameraButton() {
-        btnTakePhoto = findViewById(R.id.btnTakePhoto);
-
-        btnTakePhoto.setOnClickListener(v -> {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                    != PackageManager.PERMISSION_GRANTED) {
-                // Permission non accordée → demander
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.CAMERA},
-                        CAMERA_PERMISSION_REQUEST_CODE);
-            } else {
-                // Permission déjà accordée → lancer la caméra
-                launchCamera();
-            }
-        });
-    }
-
-    private void callApi(File file) {
-        identify_API.identificationImg(file.getAbsolutePath(), new KindwiseService.IdentificationCallback() {
-            @Override
-            public void onSuccess(IdentificationEntity identification) {
-                if (identification.getResult().getIs_mushroom().getBinary().equals("true")) {
-                    IdentifyActivity.this.mushroomIdentify = identification.getResult().
-                            getClassification().
-                            getSuggestions().get(0).getName();
-                    IdentifyActivity.this.accuracyIdentify = identification.getResult().
-                            getClassification().
-                            getSuggestions().get(0).getProbability();
-                    Intent intent_id = new Intent(IdentifyActivity.this, IdentifyDetailsActivity.class);
-                    intent_id.putExtra("scientificName", IdentifyActivity.this.mushroomIdentify);
-                    intent_id.putExtra("accuracy", IdentifyActivity.this.accuracyIdentify);
-                    file.delete();
-                    startActivity(intent_id);
-                } else {
-                    Toast.makeText(IdentifyActivity.this, "Cette photo n'est pas un champignon", LENGTH_LONG)
-                            .show();
-                }
-            }
-
-            @Override
-            public void onError(String errorMessage) {
-                Toast.makeText(IdentifyActivity.this, errorMessage, LENGTH_SHORT).show();
-                file.delete();
-            }
-        });
-    }
-
-    private void launchCamera() {
-        try {
-            photoFile = createImageFile();
-            Uri photoUri = FileProvider.getUriForFile(
-                    this,
-                    getPackageName() + ".fileprovider",
-                    photoFile
-            );
-            takePhotoLauncher.launch(photoUri);
-        } catch (IOException e) {
-            e.printStackTrace();
-            Toast.makeText(this, "Impossible de créer le fichier photo", Toast.LENGTH_SHORT).show();
-        }
-    }
     public void setToggleIconToForm() {
         if (btnToggleMode != null) {
             btnToggleMode.setImageResource(R.drawable.ic_camera);
